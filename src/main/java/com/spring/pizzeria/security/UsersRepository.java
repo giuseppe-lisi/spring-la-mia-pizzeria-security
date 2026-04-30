873258055a7ad321c8e8da13933bd03db0c50973
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-30 22:14:35 +0200

    per recuperare lo user dal db chiaramente ho bisogno di una repository

---
+++ src/main/java/com/spring/pizzeria/security/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.spring.pizzeria.security;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.spring.pizzeria.spring_la_mia_pizzeria_crud.models.User;
+
+// questa repo viene usata dal service per andare a prendere l'utente che ci serve
+public interface UsersRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
